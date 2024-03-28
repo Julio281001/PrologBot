360be5260b86b2fe8f64d97536771dc9d1a576ad
Julio Estrada <juliojr281001@gmail.com>
2024-03-28 17:54:39 -0600

    Agregar las pregunas que hará el bot

--- base_conocimiento.pl
+++ base_conocimiento.pl
@@ -1,2 +1,8 @@
+pregunta("Quieres desarrollar un sitio web?").
+pregunta("Quieres desarrollar una apliación de ios?").
+pregunta("Quieres desarrollar un proyecto de inteligencia artificial?").
+pregunta("Quieres hacer un analisis de datos?").
+pregunta("Quieres desarrollar un videojuego?").
+
 main :-
   write("Por favor, responda las siguientes preguntas."), nl,
