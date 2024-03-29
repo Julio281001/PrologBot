87ab6f9f2db3ce7b065dc9a518657cde3d0c02c6
Julio Estrada <juliojr281001@gmail.com>
2024-03-28 18:15:13 -0600

    Agregar la funcionalidad para recomendar un lenguaje

--- base_conocimiento.pl
+++ base_conocimiento.pl
@@ -4,13 +4,23 @@ pregunta("Quieres desarrollar un proyecto de inteligencia artificial?").
 pregunta("Quieres hacer un analisis de datos?").
 pregunta("Quieres desarrollar un videojuego?").
 
+recomendacion("Quieres desarrollar un sitio web?", "JavaScript").
+recomendacion("Quieres desarrollar una apliacion de ios?", "Swift").
+recomendacion("Quieres desarrollar un proyecto de inteligencia artificial?", "Python").
+recomendacion("Quieres hacer un analisis de datos?", "Python o R").
+recomendacion("Quieres desarrollar un videojuego?", "c++").
+
+recomendar(Pregunta) :-
+  recomendacion(Pregunta, Lenguaje),
+  write("Puedes utilizar "), write(Lenguaje), write(" para tu proyecto.").
+
 preguntar :-
   repeat,
   pregunta(Pregunta),
   write(Pregunta), write(" (s/n): "), read(Respuesta),
   (
     Respuesta = s -> (
-      write("Respuesta afirmativa"),
+      recomendar(Pregunta),
       !
     );
     Respuesta = salir -> !;
