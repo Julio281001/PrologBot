c4b61d96b56ecbf5f44d0ea41109352f30174ef5
Julio Estrada <juliojr281001@gmail.com>
2024-03-28 18:08:37 -0600

    Agregar la funcionalidad para preguntar las preguntas y tomar una decisión en función de la respuesta.

--- base_conocimiento.pl
+++ base_conocimiento.pl
@@ -1,8 +1,22 @@
 pregunta("Quieres desarrollar un sitio web?").
-pregunta("Quieres desarrollar una apliación de ios?").
+pregunta("Quieres desarrollar una apliacion de ios?").
 pregunta("Quieres desarrollar un proyecto de inteligencia artificial?").
 pregunta("Quieres hacer un analisis de datos?").
 pregunta("Quieres desarrollar un videojuego?").
 
+preguntar :-
+  repeat,
+  pregunta(Pregunta),
+  write(Pregunta), write(" (s/n): "), read(Respuesta),
+  (
+    Respuesta = s -> (
+      write("Respuesta afirmativa"),
+      !
+    );
+    Respuesta = salir -> !;
+    Respuesta = n -> fail
+  ).
+
 main :-
   write("Por favor, responda las siguientes preguntas."), nl,
+  preguntar.
